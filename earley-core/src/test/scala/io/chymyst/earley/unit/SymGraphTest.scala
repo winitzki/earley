747a2f1cac8ed879ca9103402fdd732fc0fb630a
Sergei Winitzki <winitzki@gmail.com>
2025-05-16 21:48:59 +0200

    wip failing ops tests

--- earley-core/src/test/scala/io/chymyst/earley/unit/SymGraphTest.scala
+++ earley-core/src/test/scala/io/chymyst/earley/unit/SymGraphTest.scala
@@ -18,7 +18,7 @@ object Fixtures1:
 
   extension (n: GraphNode)
     def &(other: GraphNode): And = And(n, other)
-    def /(other: GraphNode): Or = Or(n, other)
+    def |(other: GraphNode): Or = Or(n, other)
 
 class SymGraphTest extends FunSuite:
 
@@ -26,9 +26,9 @@ class SymGraphTest extends FunSuite:
 
   test("define some rules without cyclic dependencies") {
 
-    def a: Rule = LitStr("x") & b / c
+    def a: Rule = LitStr("x") & b | c
 
-    def b: Rule = LitStr("y") / e
+    def b: Rule = LitStr("y") | e
 
     def c: Rule = LitStr("z")
 
@@ -36,36 +36,35 @@ class SymGraphTest extends FunSuite:
 
     def e: Rule = c
 
-    val a_used = a.rulesUsed.map(_.name)
-
-    expect(a_used == Set("a", "b", "c"))
-
+    expect(a.rulesUsed.map(_.name) == Set("a", "b", "c"))
     expect(b.rulesUsed.map(_.name) == Set("b", "c"))
-
     expect(c.rulesUsed.map(_.name) == Set("c"))
     expect(d.rulesUsed.map(_.name) == Set("c", "d"))
     expect(e.rulesUsed.map(_.name) == Set("c"))
 
-    
+    expect(a.literalsUsed.map(_.toString) == Set("LitStr(x)", "LitStr(y)", "LitStr(z)"))
+    expect(b.literalsUsed.map(_.toString) == Set("LitStr(y)", "LitStr(z)"))
+    expect(c.literalsUsed.map(_.toString) == Set("LitStr(z)"))
+    expect(d.literalsUsed.map(_.toString) == Set("LitStr(z)"))
 
-    expect(a.node().toString == "And(LitStr(x),Or(Rule(b),Rule(c)))")
+    expect(a.node().toString == "Or(And(LitStr(x),Rule(b)),Rule(c))")
     expect(b.node().toString == "Or(LitStr(y),Rule(c))")
 
     val c_ops = c.opsUsed.map(_.toString)
     expect(c_ops == Set())
     val b_ops = b.opsUsed.map(_.toString)
-    expect(b_ops == Set())
+    expect(b_ops == Set("Or(LitStr(y),Rule(c))"))
     val a_ops = a.opsUsed.map(_.toString)
-    expect(a_ops == Set("And(LitStr(x),Rule(b))"))
+    expect(a_ops == Set("And(LitStr(x),Rule(b))", "Or(And(LitStr(x),Rule(b)),Rule(c))"))
 
   }
 
   test("define some rules with cyclic dependencies") {
-    def a: Rule = LitStr("x") & b / c
+    def a: Rule = LitStr("x") & b | c
 
-    def b: Rule = LitStr("y") & a / d
+    def b: Rule = LitStr("y") & a | d
 
-    def c: Rule = LitStr("z") & d / a
+    def c: Rule = LitStr("z") & d | a
 
-    def d: Rule = LitStr("w") & c / b
+    def d: Rule = LitStr("w") & c | b
   }
